befb01ab36dd5f03f7cf38a1604b7f091cbc2d26
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2023-12-13 21:07:22 +0500

    2 step

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.errorhandler(404)
+def not_found(error):
+    return 'Oops!', 404
+
+
 @app.route("/")
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
