672818e69cc1ff7d6122a30f46ac63dcd6e2aae7
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2023-12-28 14:02:40 +0500

    complete 5 step

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,7 @@
 import psycopg2
 import validators
 import os
+import requests
 from flask import Flask, render_template, request, flash, redirect, url_for, get_flashed_messages
 from dotenv import load_dotenv
 from urllib.parse import urlparse
@@ -72,6 +73,37 @@ def get_url_by_id(connection, id):
         return cursor.fetchone()
 
 
+def create_check(connection, id, status_code):
+    current_date = date.today()
+    with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+        cursor.execute(
+            """INSERT INTO url_checks (
+            url_id, status_code, h1, title, description, created_at)
+            VALUES (%s, %s, '-', '-', '-', %s);
+            """, (id, status_code, current_date,))
+
+
+def get_last_check(connection):
+    with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+        cursor.execute(
+            """SELECT DISTINCT ON (url_id)
+            id, url_id, status_code, h1, title, description, created_at
+            FROM url_checks
+            ORDER BY url_id, created_at DESC;
+            """)
+        return cursor.fetchall()
+
+
+def get_all_checks(connection, id):
+    with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+        cursor.execute(
+            """SELECT * FROM url_checks
+            WHERE url_id = %s
+            ORDER BY id DESC;
+            """, (id,))
+        return cursor.fetchall()
+
+
 @app.errorhandler(404)
 def not_found(error):
     return 'Oops!', 404
@@ -97,9 +129,13 @@ def validate(url):
 def urls():
     with connect_database(DATABASE_URL) as conn:
         urls = get_all_urls(conn)
+        url_checks = {
+            item.url_id: item for item in get_last_check(conn)
+        }
     return render_template(
         '/urls.html',
         urls=urls,
+        url_checks=url_checks
     )
 
 
@@ -125,11 +161,29 @@ def add_url():
     return redirect(url_for('url_info', id=url_id))
 
 
-@app.route("/urls/int:<id>")
+@app.route("/urls/<int:id>")
 def url_info(id):
     with connect_database(DATABASE_URL) as conn:
         url = get_url_by_id(conn, id)
+        url_checks = get_all_checks(conn, id)
     return render_template(
         'url_id.html',
         url=url,
+        checks=url_checks,
     )
+
+
+@app.post("/urls/<int:id>/checks")
+def url_check(id):
+    with connect_database(DATABASE_URL) as conn:
+        url = get_url_by_id(conn, id)
+        try:
+            request = requests.get(url.name)
+            request.raise_for_status()
+        except requests.ConnectionError:
+            flash('Произошла ошибка при проверке', 'error')
+            return redirect(url_for('url_info', id=id))
+        status_code = request.status_code
+        create_check(conn, id, status_code)
+    flash('Страница успешно проверена', 'success')
+    return redirect(url_for('url_info', id=id))
