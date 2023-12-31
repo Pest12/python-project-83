981a21e2384383c66f4efd719bbbbfc6cb326297
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2023-12-31 18:01:14 +0500

    fix bugs flake8

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -36,8 +36,8 @@ def get_seo(html):
     soup = BeautifulSoup(html, 'html.parser')
     title = soup.title.string if soup.title else ''
     h1 = soup.h1.string if soup.h1 else ''
-    description_content = soup.find('meta', attrs={'name': 'description'})
-    description = description_content.get('content') if description_content else ''
+    meta = soup.find('meta', attrs={'name': 'description'})
+    description = meta.get('content') if meta else ''
     return title, h1, description
 
 
