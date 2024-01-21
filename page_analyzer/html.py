ed9ea5a3818a5ec9e6167d79eb039f17072a19f4
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2024-01-21 21:53:08 +0500

    update html

--- page_analyzer/html.py
+++ page_analyzer/html.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 def get_seo_content(html):
     soup = BeautifulSoup(html, 'html.parser')
     title = soup.title.string if soup.title else ''
-    header_title = soup.header_title.string if soup.header_title else ''
+    header_title = soup.h1.string if soup.h1 else ''
     meta = soup.find('meta', attrs={'name': 'description'})
     description = meta.get('content') if meta else ''
     return title, header_title, description
